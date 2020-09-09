85652cc05b9b883271c4a1e1696ce388560c34b0
DanFessler <fezzinate@gmail.com>
2020-09-09 15:35:05 -0700

    .

--- src/js/main.js
+++ src/js/main.js
@@ -43,20 +43,26 @@ class Container extends Entity {
 class ParticleManager extends Entity {
   particles = [];
   container = null;
-  containerCollision = true;
-  floorCollision = false;
+  wallsCollision = false;
+  ceilingCollision = false;
   gravity = true;
+  gravityStrength = 0.02;
+  wrap = true;
+  initialVelocity = 2;
 
   constructor(count) {
     super();
+
+    let rect = canvas.getBoundingClientRect();
     this.container = Engine.game.addGameObject(
-      new Container(100, 100, 1000, 1000)
+      new Container(rect.width / 2 - 500, 100, 1000, 1000)
     );
 
-    // this.container.RectRenderer.walls.visible = false;
+    this.container.RectRenderer.walls.visible = this.wallsCollision;
+    this.container.RectRenderer.lid.visible = this.ceilingCollision;
 
     for (let i = 0; i < count / 2; i++) {
-      let v = new Vector2(Math.random() * 4, 0);
+      let v = new Vector2(Math.random() * this.initialVelocity, 0);
       v.rotate(Math.random() * (2 * Math.PI));
       let m = 4;
 
@@ -75,7 +81,7 @@ class ParticleManager extends Entity {
     }
 
     for (let i = 0; i < count / 2; i++) {
-      let v = new Vector2(Math.random() * 4, 0);
+      let v = new Vector2(Math.random() * this.initialVelocity, 0);
       v.rotate(Math.random() * (2 * Math.PI));
       let m = 4;
 
@@ -102,15 +108,15 @@ class ParticleManager extends Entity {
       // let downVec = particle.position
       //   .subtract(new Vector2(500, 500), true)
       //   .normalize()
-      //   .multiply(-0.1);
+      //   .multiply(-0.02);
 
       // Downward Gravity
-      let downVec = new Vector2(0, 0.02);
+      let downVec = new Vector2(0, this.gravityStrength);
 
       if (this.gravity) particle.RigidBody.velocity.add(downVec);
 
       // particle.position.x = this.mod(particle.position.x, 1000)
-      if (this.containerCollision) {
+      if (this.wallsCollision) {
         if (particle.position.x < this.container.position.x + particle.radius) {
           particle.position.x = this.container.position.x + particle.radius;
           particle.RigidBody.velocity.multiply(-1, 1);
@@ -123,31 +129,36 @@ class ParticleManager extends Entity {
             this.container.position.x + this.container.width - particle.radius;
           particle.RigidBody.velocity.multiply(-1, 1);
         }
-        if (particle.position.y < this.container.position.y + particle.radius) {
-          particle.position.y = this.container.position.y + particle.radius;
-          particle.RigidBody.velocity.multiply(1, -1);
-        }
-        if (
-          particle.position.y >
-          this.container.position.y + this.container.height - particle.radius
-        ) {
-          particle.position.y =
-            this.container.position.y + this.container.height - particle.radius;
-          particle.RigidBody.velocity.multiply(1, -1);
+      } else {
+        if (this.wrap) {
+          particle.position.x =
+            this.container.position.x +
+            this.mod(
+              particle.position.x - this.container.position.x,
+              this.container.width
+            );
         }
       }
 
-      if (this.floorCollision) {
-        // console.log(app.height);
-        if (particle.position.y + particle.radius > app.renderer.height) {
-          particle.position.y = app.renderer.height - particle.radius;
+      if (
+        particle.position.y >
+        this.container.position.y + this.container.height - particle.radius
+      ) {
+        particle.position.y =
+          this.container.position.y + this.container.height - particle.radius;
+        particle.RigidBody.velocity.multiply(1, -1);
+      }
+
+      if (this.ceilingCollision) {
+        if (particle.position.y < this.container.position.y + particle.radius) {
+          particle.position.y = this.container.position.y + particle.radius;
           particle.RigidBody.velocity.multiply(1, -1);
         }
       }
 
       avgVel += particle.RigidBody.velocity.length();
     }
-    console.log(avgVel / this.particles.length);
+    // console.log(avgVel / this.particles.length);
   };
 
   mod = (a, b) => ((a % b) + b) % b;
